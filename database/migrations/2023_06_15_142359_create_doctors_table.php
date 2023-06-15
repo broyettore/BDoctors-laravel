072bddd493986c61fa7328e5cb421aa2015ca661
broyettore <broyettore@gmail.com>
2023-06-15 16:39:41 +0200

    Relation user_doctor created

---
+++ database/migrations/2023_06_15_142359_create_doctors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->text("cv");
+            $table->string("address");
+            $table->text("photo");
+            $table->string("phone_number");
+            $table->string("services");
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
