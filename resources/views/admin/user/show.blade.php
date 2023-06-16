7dcd9cf0b6e85dfc2d7da176777910d0ea8d2962
Gaspare <Gas9220@users.noreply.github.com>
2023-06-16 17:14:42 +0200

    Fix show UI

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -2,55 +2,58 @@
 
 @section('content')
     <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
             <h1 class="fs-1">User #{{ $user->id }}</h1>
+
+            @if ($user->doctor)
+                <div class="d-flex">
+                    <a href="{{ route('admin.doctor.show', $user->doctor->id) }}" class="btn ms-btn-primary me-2">See Doctor
+                        Profile</a>
+                    <a href="{{ route('admin.doctor.edit', $user->doctor->id) }}" class="btn ms-btn-primary">Edit Doctor
+                        Profile</a>
+                </div>
+            @else
+                <a href="{{ route('admin.doctor.create', $user) }}" class="btn ms-btn-primary">Create Doctor Profile</a>
+            @endif
+        </div>
 
         <ul class="my-3">
             <li>First name: {{ $user->first_name }}</li>
             <li>Last Name: {{ $user->last_name }}</li>
             <li>Email: {{ $user->email }}</li>
             @if (isset($user->doctor->specialisations))
-            @foreach ( $user->doctor->specialisations as $specialisation )
-                <li>
-                    Specialisation: {{$specialisation->name}}
-                </li>
-            @endforeach
-                
+                @foreach ($user->doctor->specialisations as $specialisation)
+                    <li>
+                        Specialisation: {{ $specialisation->name }}
+                    </li>
+                @endforeach
             @endif
         </ul>
-        
-        @if ($user->doctor)
-            <div class="d-flex">
-                <a href="{{route('admin.doctor.show', $user->doctor->id)}}" class="btn ms-btn-primary me-2">See Doctor Profile</a>
-                <a href="{{route('admin.doctor.edit', $user->doctor->id)}}" class="btn ms-btn-primary">Edit Doctor Profile</a>
-            </div>
-        @else
-            <a href="{{ route('admin.doctor.create', $user) }}" class="btn ms-btn-primary">Create Doctor Profile</a>
-        @endif
 
         @if (isset($user->doctor->reviews))
-        <section>
-            <h3>Your reviews ({{ count($user->doctor->reviews) }})</h3>
-            <ul class="my-1">
-                <hr>
-                @foreach ($user->doctor->reviews as $review)
-                    <li>{{ $review->first_name . ' ' . $review->last_name }} ( {{ $review->email }})</li>
-                    <li>{{ $review->description }}</li>
+            <section>
+                <h3>Your reviews ({{ count($user->doctor->reviews) }})</h3>
+                <ul class="my-1">
                     <hr>
-                @endforeach
-            </ul>
-        </section>  
+                    @foreach ($user->doctor->reviews as $review)
+                        <li>{{ $review->first_name . ' ' . $review->last_name }} ( {{ $review->email }})</li>
+                        <li>{{ $review->description }}</li>
+                        <hr>
+                    @endforeach
+                </ul>
+            </section>
         @endif
         @if (isset($user->doctor->votes))
-        <section>
-            <h3>Your valutation ({{ count($user->doctor->votes) }})</h3>
-            <ul class="my-1">
-                <hr>
-                @foreach ($user->doctor->votes as $vote)
-                    <li>{{ $vote->value }}</li>
+            <section>
+                <h3>Your valutation ({{ count($user->doctor->votes) }})</h3>
+                <ul class="my-1">
                     <hr>
-                @endforeach
-            </ul>
-        </section>  
+                    @foreach ($user->doctor->votes as $vote)
+                        <li>{{ $vote->value }}</li>
+                        <hr>
+                    @endforeach
+                </ul>
+            </section>
         @endif
     </div>
 @endsection
