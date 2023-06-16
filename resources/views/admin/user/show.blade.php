36d7cb388910b6ce3149144445ec23e5aef2743a
Gaspare <Gas9220@users.noreply.github.com>
2023-06-16 11:12:08 +0200

    User show, improver UI and added review section

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -2,16 +2,29 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mb-3 fs-1">User #{{ $user->id }}</h1>
-        <ul class="mb-3">
+        <div class="d-flex justify-content-between align-items-center">
+            <h1 class="fs-1">User #{{ $user->id }}</h1>
+            @if ($user->doctor)
+                <a href="{{ route('admin.doctor.edit', $user->doctor->id) }}" class="btn btn-primary">Edit Doctor Profile</a>
+            @else
+                <a href="{{ route('admin.doctor.create', $user) }}" class="btn ms-btn-primary">Create Doctor Profile</a>
+            @endif
+        </div>
+        <ul class="my-3">
             <li>First name: {{ $user->first_name }}</li>
             <li>Last Name: {{ $user->last_name }}</li>
             <li>Email: {{ $user->email }}</li>
         </ul>
-        @if ($user->doctor)
-            <a href="{{route('admin.doctor.edit', $user->doctor->id)}}" class="btn btn-primary">Edit Doctor Profile</a>
-        @else
-            <a href="{{route('admin.doctor.create', $user)}}" class="btn ms-btn-primary">Create Doctor Profile</a>
-        @endif
+        <section>
+            <h3>Your reviews ({{ count($user->doctor->reviews) }})</h3>
+            <ul class="my-1">
+                <hr>
+                @foreach ($user->doctor->reviews as $review)
+                    <li>{{ $review->first_name . ' ' . $review->last_name }} ( {{ $review->email }})</li>
+                    <li>{{ $review->description }}</li>
+                    <hr>
+                @endforeach
+            </ul>
+        </section>
     </div>
 @endsection
