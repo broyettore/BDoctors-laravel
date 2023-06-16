353d5d88fad67a39bc8d3747db4413a3e85ba70e
broyettore <broyettore@gmail.com>
2023-06-16 17:20:18 +0200

    feat seeder doctor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Doctor;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            // DoctorSeeder goes here
+            DoctorSeeder::class,
             ReviewSeeder::class,
             VoteSeeder::class
         ]);
