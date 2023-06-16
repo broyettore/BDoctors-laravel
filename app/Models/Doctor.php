91d08e6d71b5215d766773b072a9b5d9d0e00268
Gaspare <Gas9220@users.noreply.github.com>
2023-06-16 10:48:22 +0200

    Added review relationship on Doctor Model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Doctor extends Model
 {
@@ -13,5 +13,10 @@ class Doctor extends Model
 
     public function user():BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
     }
 }
