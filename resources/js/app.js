3fef861dc9b9080a1f15cb8593ab62d8bf2f4d3a
Gaspare <Gas9220@users.noreply.github.com>
2023-06-19 13:08:22 +0200

    Buy Sponsorships layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,14 @@ import.meta.glob([
     '../img/**'
 ]);
 
+const sponsorshipBoxes = document.querySelectorAll('.sponsorship-box');
+const sponsorshipOptions = document.querySelectorAll('.sponsorship-option');
+
+sponsorshipOptions.forEach(sponsorship => {
+    sponsorship.addEventListener('change', function () {
+        sponsorshipOptions.forEach(sponsorship => {
+            sponsorship.parentElement.classList.remove('sponsorship-selected');
+        });
+        sponsorship.parentElement.classList.add('sponsorship-selected');
+    });
+});
