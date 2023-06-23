16406eef0fc9c63201c44b83aa60f6ee2ca5f5ef
broyettore <broyettore@gmail.com>
2023-06-23 16:45:40 +0200

    design corresponds to front-end now

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,8 @@
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card ms-login-card">
+                <div class="card-header ms-login-head">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,8 @@
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="card ms-register ms-register">
+                <div class="card-header ms-register-head">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
